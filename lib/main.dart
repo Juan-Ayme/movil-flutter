7e6e0ea4d0e71187d1983d7c825f11e5b007ac07
Maver -- <Ayme Juan>
2023-11-29 12:06:20 -0500

    agregando appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,18 @@
 import 'package:flutter/material.dart';
-void main(){
-  
+import 'package:proyecto_movil_a1/presentation/screens/counter/counter_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:CounterScreen(),
+    );
+  }
 }
